6e5dc7c85804b4286c37062f0c0f609253362c64
Simon <simonmarkholland@gmail.com>
2019-03-08 10:34:13 +0700

    Add home and splash routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
+var text = "Some quick example text to build on the card title and make up the bulk of the card's content."
+
 
 router.get('/', function(req, res){
     res.render('landing');
+});
+
+router.get('/home', function(req, res){
+    var sections = [
+        {title: "Heading 1", image: "imgs/coloring.png", paragraph: text},
+        {title: "Heading 2", image: "imgs/apples.png", paragraph: text},
+        {title: "Heading 3", image: "imgs/glasses.png", paragraph: text}
+    ]
+
+    res.render('home', {articles: sections});
 });
 
 module.exports = router;
