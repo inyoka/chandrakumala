995ececc526d84be4cf2556696fe09fef0b0eb85
Simon <simonmarkholland@gmail.com>
2019-03-18 13:50:56 +0700

    Update app.js with all require() statements

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
 var express = require("express");
-var app = express();
-var indexRoutes = require("./routes/index");
+var app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Page = require("./models/page"),
+    Section = require("./models/section"),
+    User = require("./models/user");
 
 require('dotenv').config();
 
+
+//requiring routes
+var sectionRoutes = require("./routes/sections"),
+    pageRoutes = require("./routes/pages"),
+    indexRoutes = require("./routes/index")
+
+var databaseurl = process.env.DATABASEURL || 'mongodb://localhost/chandrakumala_01';
+mongoose.connect(databaseurl, { useNewUrlParser: true });
+
+// Stops deprecation warning about collection.findAndModify
+mongoose.set('useFindAndModify', false);
+
+
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
-app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(flash());
+app.set('view engine', 'ejs');
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
 
 app.use("/", indexRoutes);
+app.use("/pages", pageRoutes);
+app.use("/sections/:id/sections", sectionRoutes);
 
 // Use Environmental variables to determine PORT and IP to liston on...
 
@@ -15,5 +63,5 @@ var listener = app.listen(process.env.PORT, process.env.IP, function(){
     var address = listener.address().address;
     var port = listener.address().port;
   
-    console.log('YelpCamp server listening on : ' + address + ':' + port);
+    console.log('ChandraKumala server listening on : ' + address + ':' + port);
   });
