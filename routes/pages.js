1c37fdb4b4a8ec56525f16a8f14e472729d4aa8e
Simon <simonmarkholland@gmail.com>
2019-03-19 12:29:52 +0700

    Remove price field

--- routes/pages.js
+++ routes/pages.js
@@ -25,7 +25,6 @@ router.post('/', middleware.isLoggedIn, function(req, res){
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var price = req.body.price;
   var author = {
     id: req.user._id,
     username: req.user.username
