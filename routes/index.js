2ee5721f7abc6c3bd6d679b749f0be54697c7c42
Simon <simonmarkholland@gmail.com>
2019-03-19 11:04:40 +0700

    Add auth routes and templates

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+var middleware = require("../middleware");
+
 
 var text = "Some quick example text to build on the card title and make up the bulk of the card's content."
 
@@ -18,8 +22,49 @@ router.get('/home', function(req, res){
     res.render('home', {articles: sections});
 });
 
-router.post('/home', function(req, res){
-    
-})
+// AUTH ROUTES
+
+// Register form
+router.get('/register', function(req, res){
+    res.render('register');
+  });
+
+// Registration
+router.post('/register', function(req, res){
+    var newUser = new User({username: req.body.username});
+  //   eval(require('locus'))
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+          req.flash("error", err.message);
+          return res.redirect("/register");
+        }
+        passport.authenticate("local")(req, res, function(){
+          req.flash("success", "Welcome to YelpCamp " + user.username);
+          res.redirect("/pages");
+        });
+    });
+  });
+  
+  // Login form
+  router.get('/login', function(req, res){
+    res.render('login');
+  });
+  
+  // Login
+  router.post('/login', passport.authenticate("local",
+   {
+     successRedirect: "/pages",
+     failureRedirect: "/login"
+   }), function(req, res){
+  });
+  
+  
+  // Logout Route
+  router.get('/logout', middleware.isLoggedIn, function (req, res){
+    req.logout();
+    req.flash("success", "Logged you out!!");
+    res.redirect('/pages');
+  });
+
 
 module.exports = router;
